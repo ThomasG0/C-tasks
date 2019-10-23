457fd39c65d82cf9463ba7d2ecd75a0a2a2e3437
cmc <g4mg152@gmail.com>
2019-10-23 18:31:02 +0300

    OK

---
+++ Task3/Task.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <memory>
+
+class A {
+	public:
+	static void * operator new(size_t size) {
+		std::cout << "operator new!" << std::endl;
+		return ::operator new(size);
+	}
+	static void operator delete(void *p, size_t size) {
+		std::cout << "operator delete!" << std::endl;
+		return ::operator delete(p);
+	}
+};
+
+template<class T,class S>
+struct g_allocator
+{
+	typedef T value_type;
+    g_allocator()
+    {}
+    template <class Pos>
+		g_allocator (const g_allocator<Pos,Pos>&)
+		{}
+    value_type* allocate (size_t vol) {
+        return reinterpret_cast<value_type*>(S::operator new(vol*sizeof(value_type)));
+    }
+    void deallocate (value_type* elem, size_t vol) {
+        ::operator delete(elem);
+    }
+};
+
+int main() {
+	auto sp = std::make_shared<A>();
+	auto sps = std::allocate_shared<A>(g_allocator<A,A>());
+}
