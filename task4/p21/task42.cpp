4593d3ab41cad96f1c7c7a2e989771ec8b2c943e
cmc <g4mg152@gmail.com>
2019-11-29 18:43:49 +0300

    taks_4_2_wttype

--- task4/p21/task42.cpp
+++ task4/p21/task42.cpp
@@ -16,42 +16,43 @@ template<>
 class are_same<>
 {
 public:
-	bool v = true;
+	bool value = true;
 };
 
 template<class A>
 class are_same<A>
 {
 public:
-	bool v = true;
+	bool value = true;
 };
 
 template<class A, class B>
 class are_same<A,B>
 {
 public:
-	bool v = std::is_same<A,B>::value;
+	bool value = std::is_same<decay_t<A>,decay_t<B>>::value;
 };
 
 template<class A, class B, class C>
 class are_same<A,B,C>
 {
 public:
-	bool v = std::is_same<A,B>::value && std::is_same<B,C>::value;
+	bool value = std::is_same<decay_t<A>,decay_t<B>>::value && std::is_same<decay_t<B>,decay_t<C>>::value;
 };
 
 template<class A, class B, class... Args>
 class are_same<A, B, Args...>: public are_same<Args...>
 {
 public:
-	constexpr are_same(){this->v = this->v && std::is_same<A,B>::value;}
+	constexpr are_same(){this->value = this->value && std::is_same<decay_t<A>,decay_t<B>>::value;}
 };
 
 template<class...Args>
-constexpr bool are_same_v = are_same<Args...>().v;
+constexpr bool are_same_v = are_same<Args...>().value;
 
 int main(void)
 {
-		static_assert(are_same_v<int,int>, "compile assert");
+	static_assert(are_same_v<int,int>, "compile assert");
+	//static_assert(are_same_v<int, int32_t, signed int>, "compile assert");
 	return 0;
 }
